0534fc8d063d706ebd7d4507d0f6ff39f3fb7a0f
Jason <zzanggu1988@gmail.com>
2023-02-10 00:51:01 +0900

    2-8DELETECard

--- lamda/deleteCard.js
+++ lamda/deleteCard.js
@@ -9,21 +9,15 @@ exports.handler = async event => {
     console.log("Received : " + JSON.stringify(event, null, 2));
     let response = "";
     try{
-        const id = event.pathParameters.id;
-        const body = JSON.parse(event.body);     
+        const id = event.pathParameters.id;   
         var params = {
-            TableName: tableName,
-            Key: { id :  id},
-            UpdateExpression: 'set #c = :c, #t = :t',   //#c : category, #t : title
-            //ConditionExpression: '#a < :MAX', 여기서 사용안해서 주석
-            ExpressionAttributeNames: {'#c' : "category", "#t" : "title"},
-            ExpressionAttributeValues: {
-              ':c' : body.category,
-              ':t' : body.title,
+            TableName : tableName,
+            Key: {
+              "id": id
             }
           };
-       
-        await documentClient.update(params).promise();
+      
+        await documentClient.delete(params).promise();
 
         response = {
             statusCode: 200
