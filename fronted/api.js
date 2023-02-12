56318c091657b35570a8fe424e339b2be38b1bf2
Jason <zzanggu1988@gmail.com>
2023-02-12 12:53:56 +0900

    3-3PostCard

--- fronted/api.js
+++ fronted/api.js
@@ -17,14 +17,17 @@ export default class APIHandler {
 
   // TODO: 카드 객체 생성/추가 후 ID 반환
   async postCard(cardObj) {
-    let id = Math.round(Math.random() * 10000).toString();
-    this.dummyData.push({
-      id: id,
-      title: cardObj.title,
-      category: cardObj.category
+    const request = new APIRequest("POST", "/kanban/cards",{
+      title : cardObj.title, 
+      category : cardObj.category
     });
-    console.log(this.dummyData);
-    return id;    
+    const response = await APIProcessor(request);
+    if(response !== "Error"){
+      console.log(response);
+      return response.id;
+    }else{
+      return null;
+    }
   }
 
   // TODO: ID로 카드 검색 후 내용,카테고리 수정
